dbaa4c34c79c85e40a4ebe755369ba0f76fd10c4
OwethuSotomela <owethusotomela@gmail.com>
2022-04-16 13:40:55 +0200

    Created main content

---
+++ src/MainContent.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import ReactDOM  from "react-dom";
+
+export default function Page(){
+    return(
+        <div>
+            <img src="./"alt="" />
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by John Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
