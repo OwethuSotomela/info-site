aac7acc245e616eef990428afefe25a5c5eddc78
OwethuSotomela <owethusotomela@gmail.com>
2022-04-17 12:46:00 +0200

    Adding styling to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from "react";
-import ReactLogo from "../images/react-logo1.png";
+import ReactLogo from "../images/react-logo2.png";
 
 export default function Header() {
     return (
         <header>
             <nav className="navbar">
                 <img src={ReactLogo} alt="" className="nav-logo" />
-                <ul className="nav-items">
+                <h3 className="nav--h3">ReactFacts</h3>
+                <h4 className="nav--h4">React Project 1</h4>
+                {/* <ul className="nav-items">
                     <li>Project 1</li>
                     <li>Project 2</li>
                     <li>Project 3</li>
-                </ul>
+                </ul> */}
             </nav>
         </header>
     )
