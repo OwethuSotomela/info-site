392b9f58196ecea07a34b7771e0656b88598ed79
OwethuSotomela <owethusotomela@gmail.com>
2022-04-17 15:36:14 +0200

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import ReactDOM from "react-dom";
-// import ""
 
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 
 export default function Footer() {
     return (
-        <div>
+        <footer className="footer">
             © <small>Info-Site (2022) O Sotomela development. All rights reserved.</small>
-        </div>
+        </footer>
     )
 }
